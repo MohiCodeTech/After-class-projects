5addbac1584a832557ffbdbd522a9805759d4280
MohiCodeTech <moheath32kg@gmail.com>
2025-03-16 20:06:55 +0530

    P 19

---
+++ Project 19/AboutMe.js
@@ -0,0 +1,10 @@
+console.log("Hello! My name is Moheathraam, how are you?")
+console.log("I am 14 years old and my ambition is to become a software engineer!")
+let a = 24;
+let b = 12;
+
+let sum = a + b
+console.log(sum)
+
+let sub = a - b
+console.log(sub)
