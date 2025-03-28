1e416a763a57f51d8010b587d6ec635c1c6ef512
MohiCodeTech <moheath32kg@gmail.com>
2025-03-28 17:36:45 +0530

    P 21

---
+++ Project 21/MethodsSwitches.js
@@ -0,0 +1,37 @@
+//Loops
+for(i = 1 ; i <= 5; i++){
+    console.log("Welcome to my Program!")
+}
+
+//Cases and swith
+let time = "1pm"
+
+switch(time){
+    case "6am":
+    console.log("Start of the Day! Have a nice Start! ")
+    break;
+
+    case "1pm":
+        console.log("Half of the day finished! Have some lunch to be refreshed")
+        break;
+
+    case "5pm":
+        console.log("have some tea with snacks!")
+        break;
+
+    case "8pm":
+        console.log("Have the final meal of the Day!")
+        break;
+
+    case "10pm":
+        console.log("time to have a good day's rest!")
+        break;
+
+     default:
+        console.log("A normal time! Don't give up!")
+        break;
+}
+
+//Methods
+console.log(Math.random())
+console.log(Math.PI)
