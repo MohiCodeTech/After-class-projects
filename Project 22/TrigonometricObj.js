55a3024c88e7670b1fdd6c68c50c27ca27451830
MohiCodeTech <moheath32kg@gmail.com>
2025-03-29 21:24:44 +0530

    P 22

---
+++ Project 22/TrigonometricObj.js
@@ -0,0 +1,25 @@
+//Objects
+let student1 = {
+    Name: "Moheathraam",
+    Age: 14,
+    skills: "Coding"
+}
+
+let student2 = {
+    Name: "Michael",
+    Age: 13,
+    skills: "Football, Basketball"
+}
+//Keys of Objects in console.log
+console.log("Student1 name is " + student1.Name)
+console.log("Student1 age is " + student1.Age)
+console.log("Student1 skill is " + student1.skills)
+
+console.log("\nStudent2 name is " + student2.Name)
+console.log("Student2 age is " + student2.Age)
+console.log("Student2 skills are " + student2.skills)
+
+//Trigonometric Numbers
+console.log("\nThis is the tan of 1: " + Math.tan(1))
+console.log("This is the cos of 1: " + Math.cos(1))
+console.log("This is the sin of 1: " + Math.sin(1))
