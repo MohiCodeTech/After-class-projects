5f65794b04cd1f79b53107900bcdfaddce4704b9
MohiCodeTech <moheath32kg@gmail.com>
2025-04-18 12:13:39 +0530

    P 27

---
+++ Project 27/ArraySortingProject.js
@@ -0,0 +1,17 @@
+let number = [1, 3, 44, -4, 9.1,]
+let dscnumbers = number.sort((a, b) =>  b - a)
+console.log(dscnumbers)
+
+
+let ascnumbers = number.sort((a, b) => a - b)
+console.log(ascnumbers)
+
+let newnumbers = number.map((num) => num / 2)
+console.log(newnumbers)
+
+//assorting alphebet 
+let favfoods = ["Spaghetti", "Pizza", "Burger", "Dosa", "Cake"]
+console.log(favfoods)
+
+let asofavfoods = favfoods.sort()
+console.log(asofavfoods)
