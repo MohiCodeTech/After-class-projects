aaf6a8a26608babd013b62f93bf47d9efc5637ec
MohiCodeTech <moheath32kg@gmail.com>
2025-05-31 12:56:22 +0530

    P 36

---
+++ Project 36/Input.py
@@ -0,0 +1,16 @@
+str1 = "Hello, "
+str2 = "User!"
+combined = str1 + str2
+print(combined)
+name = input("May I know your name?: ")
+if name.upper() == "MOHEATHRAAM":{
+      print("Yo, Wsup Dude!")
+}
+else:
+    print("Nice name!")
+age = int(input("What is your age?: "))
+if age >= 18:{
+    print("Wow You are old and also you can do many things such as driving and voting!")
+}
+else:
+    print("You are still young, There is a lot of time so enjoy it!")
