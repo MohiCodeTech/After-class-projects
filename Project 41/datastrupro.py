ac924084867ce81b8202a1780c7cdf0b116418d8
MohiCodeTech <moheath32kg@gmail.com>
2025-06-07 08:49:50 +0530

    Project 41

---
+++ Project 41/datastrupro.py
@@ -0,0 +1,23 @@
+friends = ["Sanjeeth", "Vel", "Umesh", "saif"]
+Sanjeeth = {
+    "full name" : "Sanjeeth",
+    "Age" : 14,
+    "skills" : "very good at math"
+}
+Vel = {
+    "full name" : "Vel",
+    "Age" : 13,
+    "skills" : "good in games"
+}
+Umesh = {
+    "full name" : "Umesh",
+    "Age" : 14,
+    "skills" : "worst in everything"
+}
+Saif = {
+    "full name" : "Saif",
+    "Age" : 13,
+    "skills" : "very funny"
+}
+
+print("My friends info are", Sanjeeth , Saif, Vel, Umesh)
