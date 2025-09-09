60bcb2fb629027b702677e47ed571522dba8ae08
MohiCodeTech <moheath32kg@gmail.com>
2025-09-09 19:05:09 +0530

    prolects

--- Projects for Bits/project5.py
+++ Projects for Bits/project5.py
@@ -1,1 +1,13 @@
 #Longetst1s
+n = int(input("Enter a number to find its longest consecutive 1s: "))
+current_count = 0  
+max_count = 0
+while n > 0:
+    if n & 1:
+        current_count +=1
+        max_count = max(max_count, current_count)
+    else:
+        current_count = 0
+    n >>= 1
+
+print("longest consecutive ones: ", max_count)
--- Projects for Bits/project6.py
+++ Projects for Bits/project6.py
@@ -1,16 +1,14 @@
 def powerset(stringset, stringsetsize):
     power_set_size = 2**stringsetsize
-    outer = 0
-    inner = 0
-    for outer in range(0 , power_set_size):
+    for outer in range(power_set_size):
         subset = ""
-        for inner in range(0, stringsetsize):
+        for inner in range(stringsetsize):
             if (outer & (1 << inner)) > 0:
-                subset +=  str(stringset[inner]) + ""
+                subset +=  str(stringset[inner]) + " "
 
         print("{", subset, "}")
 
 stringset = input("Enter a name to see all its substrings: ")
-stringsetsize = int(input("Enter the amout of letter it has: "))
-set = ["" + stringset]
-powerset(set, stringsetsize)
+chars = list(stringset)
+stringsetsize = len(stringset)
+powerset(stringset, stringsetsize)
